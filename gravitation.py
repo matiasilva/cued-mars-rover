f9dcdc81cbcb936703014e0118c54ad3dc5641d6
Matias Silva <mw781@srcf.net>
2020-09-07 16:13:47 +0100

    Finish task 2

--- gravitation.py
+++ gravitation.py
@@ -2,6 +2,7 @@
 # %matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
+from math import sqrt
 
 
 def do_integration(x_0, v_0, func):
@@ -24,9 +25,9 @@ def do_integration(x_0, v_0, func):
 
             # calculate new position and velocity for next iteration
             if t == 0:
-                a, x, v = do_euler(x, v, t)
+                x, v = do_euler(x, v, t)
             else:
-                a, x, v = func(x, v, x_list[-2])
+                x, v = func(x, v, x_list[-2])
 
     return (x_list, v_list)
 
@@ -37,7 +38,7 @@ def do_euler(x, v, *argv):
     a = (-G * M / (mag_x ** 2)) * (x / mag_x)
     x = x + dt * v
     v = v + dt * a
-    return (a, x, v)
+    return (x, v)
 
 
 def do_verlet(x, v, *argv):
@@ -47,37 +48,52 @@ def do_verlet(x, v, *argv):
     a = (-G * M / (mag_x ** 2)) * (x / mag_x)
     x = 2 * x - argv[0] + (dt ** 2) * a
     v = (x - px) / dt
-    return (a, x, v)
+    return (x, v)
+
+
+def get_component(arr, axis, index):
+    return np.apply_along_axis(lambda a_1d: a_1d[index], axis, arr)
+
+
+def get_xy(arr):
+    return (get_component(arr, 1, 0), get_component(arr, 1, 1))
 
 
 # mass, gravitational constant, initial position and velocity
 M = 6.42 * 10 ** 23
 G = 6.67 * 10 ** (-11)
 MARS_RAD = 3386000
-pos = [0, MARS_RAD + 30000, 0]
-vel = [0, 0, 0]
+HEIGHT_ABOVE_SURFACE = 30000
+TOTAL_HEIGHT = MARS_RAD + HEIGHT_ABOVE_SURFACE
+mag_vel_circ = sqrt(G * M / TOTAL_HEIGHT)
+mag_vel_escape = sqrt(2) * mag_vel_circ
+pos = [0, TOTAL_HEIGHT, 0]
+vel = [mag_vel_circ, 0, 0]
 
 # simulation time, timestep and time
-t_max = 100
+t_max = 5000
 # critical value 1.7-1.8
 dt = 0.1
 t_array = np.arange(0, t_max, dt)
 
-euler = do_integration(pos, vel, do_euler)
+# euler = do_integration(pos, vel, do_euler)
 verlet = do_integration(pos, vel, do_verlet)
 
-euler_mag = np.apply_along_axis(np.linalg.norm, 1, euler[0])
-verlet_mag = np.apply_along_axis(np.linalg.norm, 1, verlet[0])
+# euler_mag = np.apply_along_axis(np.linalg.norm, 1, euler[0])
+# verlet_mag = np.apply_along_axis(np.linalg.norm, 1, verlet[0])
+
+# euler_x, euler_y = get_xy(euler[0])
+verlet_x, verlet_y = get_xy(verlet[0])
 
 # plot the position-time graph
-plt.figure(1)
+plt.figure(1, (6.4, 6.4))
 plt.clf()
-plt.xlabel("time (s)")
-plt.ylabel("norm of x/v")
+plt.xlabel("x (m)")
+plt.ylabel("y (m)")
 plt.grid()
-plt.ticklabel_format(useOffset=False, style='plain')
+plt.ticklabel_format(useOffset=False, style="plain")
 # plt.ylim(bottom=-10, top=10)  set the ylim to bottom, top
-plt.plot(t_array, euler_mag, label="x (m) euler")
-plt.plot(t_array, verlet_mag, label="x (m) verlet")
+# plt.plot(euler_x, euler_y, label="euler")
+plt.plot(verlet_x, verlet_y, label="verlet")
 plt.legend()
 plt.show()
